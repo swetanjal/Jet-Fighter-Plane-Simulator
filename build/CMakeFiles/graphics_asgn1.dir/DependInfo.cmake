f5dd234a28efa4a3e84303ff84a42972f886239f
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2019-02-09 23:07:24 +0530

    Set up boilerplate code for Assignment.

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tapas/workspace/Repositories/graphics-boilerplate/src/ball.cpp" "/home/tapas/workspace/Repositories/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/tapas/workspace/Repositories/graphics-boilerplate/src/color.cpp" "/home/tapas/workspace/Repositories/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/tapas/workspace/Repositories/graphics-boilerplate/src/input.cpp" "/home/tapas/workspace/Repositories/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/tapas/workspace/Repositories/graphics-boilerplate/src/main.cpp" "/home/tapas/workspace/Repositories/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/tapas/workspace/Repositories/graphics-boilerplate/src/nonedit.cpp" "/home/tapas/workspace/Repositories/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/tapas/workspace/Repositories/graphics-boilerplate/src/other_handlers.cpp" "/home/tapas/workspace/Repositories/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/tapas/workspace/Repositories/graphics-boilerplate/src/timer.cpp" "/home/tapas/workspace/Repositories/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/ball.h
@@ -0,0 +1,22 @@
+#include "main.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#ifndef BALL_H
+#define BALL_H
+
+
+class Ball {
+public:
+    Ball() {}
+    Ball(float x, float y, color_t color, double SPEED);
+    glm::vec3 position;
+    float rotation;
+    void draw(glm::mat4 VP);
+    void set_position(float x, float y);
+    void tick();
+    double speed;
+    double gravity;
+private:
+    VAO *object;
+};
+
+#endif // BALL_H
